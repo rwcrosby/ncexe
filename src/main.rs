182324bef1576d0ba36c35f116dab731dac7af5a
Ralph Crosby <rwc@cs.tamu.edu>
2023-11-24 20:11:55 -0500

    Figuring out exe type

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,89 @@
 use clap::Parser;
+use memmap2::Mmap;
+use std::io::{Error, Result};
+use std::fs::File;
 
+/// Display executable file information
 #[derive(Parser,Default,Debug)]
 struct Arguments {
-    exe_filename: String
+
+    /// Name of the executable file(s)
+    #[arg(required=true)]
+    exe_filename: Vec<String>
 }
 
 #[derive(Debug)]
 struct Mapfile<'a> {
-    filename: String,
-    addr: Option<&'a u8>,
-    len: u64,
+    filename: &'a str,
+    mmap: Mmap,
 }
 
 impl<'a> Mapfile<'a> {
-    fn new(filename: String) -> Result<Self, &'static str> {
+    fn new(filename: &'a str) -> Result<Self> {
+
+
+        let file = File::open(filename)?;
+        let mmap = unsafe { Mmap::map(&file)? };
+
         Ok(Self{filename: filename, 
-            addr: None,
-            len: 0})
+                mmap: mmap
+                }
+            )
         }
     }
     
-    #[derive(Debug)]
-    struct Configuration {
-
+#[derive(Debug)]
+enum ExeType {
+    MachO32,
+    MachO64,   
+    UNIVBIN,   
+    ELF,   
 }
 
-impl Configuration {
-    fn new(args : & Arguments) -> Result<Self, &'static str> {
-        Ok(Self{})
+impl ExeType {
+    fn set(mmap: &Mmap) -> Result<Self> {
+
+        if mmap.len() < 4 {
+            return Err(Error::other(format!("File too short: {}", mmap.len())))
+        }
+
+        let raw_type = unsafe{ *(mmap.as_ptr() as *const u32) };
+        match raw_type {
+            0xfeedfacf => Ok(ExeType::MachO64),
+            0xfeedface => Ok(ExeType::MachO32),
+            0xcafebabe => Ok(ExeType::UNIVBIN),
+            0x7f454c46 => Ok(ExeType::ELF),
+            v => Err(Error::other(format!("Invalid magic number: {:x}", v))),
+        }
+
     }
 }
 
 fn main() {
 
     // Process the arguments
-
     let args = Arguments::parse();
-    println!("{:?}", args);
     
-    // Setup the configuration
-    
-    let config = Configuration::new(&args).unwrap();
-    println!("{:?}", config);
-
     // Map the executable
     
-    let mapped_file = Mapfile::new(args.exe_filename).unwrap();
+    // let mapped_file = Mapfile::new(&args.exe_filename).expect("Unable to map file: ");
+    let mapped_file = match Mapfile::new(&args.exe_filename[0]) {
+        Ok(v) => v,
+        Err(e) => panic!("Unable to map file: {}", e.to_string())
+    };
     println!("{:?}", mapped_file);
 
     // Dump the executable
+
+    println!("{} {:?}", mapped_file.filename, mapped_file.mmap);
+
+    // Get the executable type
+
+    let exe_type = match ExeType::set(&mapped_file.mmap) {
+        Ok(v) => v,
+        Err(e) => panic!("Bad magic number: {}", e.to_string())
+    };
+
+    println!("{:?}", exe_type);
 
 }
--- tests/bytes.rs
+++ tests/bytes.rs
@@ -1,6 +1,9 @@
 #![allow(unused)]
 
+// https://stackoverflow.com/questions/59289331/how-do-i-map-data-from-a-vector-of-bytes-to-a-structure-in-rust
+
 #[test]
+// align_to doesn't move the data, just sets up another slice
 pub fn align() {
     
     let bytes: [u8; 7] = [1, 2, 3, 4, 5, 6, 7];
@@ -9,7 +12,7 @@ pub fn align() {
     unsafe {
         bytes.align_to::<u16>()
     };
-    println!("{:?} - {:?} - {:?}", prefix, shorts, suffix);
+    println!("Align: {:?} - {:?} - {:?}", prefix, shorts, suffix);
     
     #[repr(C, packed)]
     #[derive(Debug, Copy, Clone)]   
@@ -20,9 +23,63 @@ pub fn align() {
     
     let (head, body, tail) = unsafe { bytes.align_to::<MyStruct>() };
     assert!(head.is_empty(), "Data was not aligned");
-    println!("{:?} - {:?} - {:?}", head, body, tail);
+    println!("Align: {:?} - {:?} - {:?}", head, body, tail);
     let my_struct = &body[0];
     
-    println!("{:?}", my_struct);
+    println!("Align: {:?}", my_struct);
     
+}
+
+#[test]
+// transmute creats a copy of the dataq
+pub fn transmute() {
+    
+    let bytes: [u8; 7] = [1, 2, 3, 4, 5, 6, 7];
+    
+    #[repr(C, packed)]
+    #[derive(Debug, Copy, Clone)]   
+    struct MyStruct {
+        foo: u16,
+        bar: u8,
+        baz: [u8; 4],
+    }    
+    
+    let my_struct = unsafe {
+        std::mem::transmute::<[u8; 7], MyStruct> (bytes)
+    };
+
+    println!("Transmute: {:?}", my_struct);
+    
+}
+
+#[test]
+// raw_points seem to be the way to go, no data movement, just reference to the memory
+// read_unaligned moves the data
+pub fn raw_pointer() {
+    
+    let bytes: [u8; 7] = [1, 2, 3, 4, 5, 6, 7];
+    
+    #[repr(C, packed)]
+    #[derive(Debug, Copy, Clone)]   
+    struct MyStruct {
+        foo: u16,
+        bar: u8,
+        baz: [u8; 4],
+    }    
+    
+    let my_struct = unsafe {
+        (bytes.as_ptr() as *const MyStruct).read_unaligned() 
+    };
+
+    println!("Read_unaligned: {:?}", my_struct);
+    
+    let my_struct_ptr = bytes.as_ptr() as *const MyStruct;
+    
+    let my_struct_ptr2 = unsafe{ *my_struct_ptr };
+    let my_struct_ptr3 = unsafe{ *(bytes.as_ptr() as *const MyStruct) };
+
+    println!("Read_unaligned: {:?}", my_struct_ptr);
+    println!("Read_unaligned: {:?}", my_struct_ptr2);
+    println!("Read_unaligned: {:?}", my_struct_ptr3);
+
 }
