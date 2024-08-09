ceff84c68bf705c3fd31dd6510117084c4e3f204
Ralph W Crosby <rcrosby@camgian.com>
2024-08-09 09:20:01 -0400

    Trait vectors example

---
+++ examples/trait_vector.rs
@@ -0,0 +1,52 @@
+
+
+struct MyStruct<'s> {
+    s: String,
+    ss: &'s str,
+}
+    
+impl<'ms> MyStruct<'ms> {
+    
+    fn new(s: &'ms str) -> Box<dyn MyTrait<'ms> + 'ms> {
+        Box::new(Self{s: s.to_string() + " as string",
+            ss: s
+        })
+    }
+    
+}
+
+trait MyTrait<'t>: {
+    fn some_fn(&self) {
+        println!("In SomeFn")
+    }
+}
+
+impl MyTrait<'_> for MyStruct<'_> {
+    fn some_fn(&self) {
+        println!("In SomeFn for MyStuct s:{}, ss:{}", self.s, self.ss)
+    }
+}
+
+fn receiver<'t>(my_trait: &dyn MyTrait<'t>) {
+    my_trait.some_fn();
+}
+
+type MyTraitRef<'t> = dyn MyTrait<'t>;
+
+fn main() {
+    
+    let ms = Box::new(MyStruct{s: "ms".to_string(), ss: "sliceme"});
+    let ms2 = MyStruct::new("Blah");
+    
+    let trait_ref = ms2.as_ref();
+    receiver(trait_ref);
+
+    ms.some_fn();
+    ms2.some_fn();
+
+    let msv: Vec<Box<dyn MyTrait>> = vec![ms, ms2];
+ 
+    receiver(msv[0].as_ref());
+    
+    println!("Do something else");
+}
