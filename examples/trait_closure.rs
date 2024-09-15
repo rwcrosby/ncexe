346cb3485253c4d5fc45c2b7672acdb745fe04f4
Ralph W Crosby <rcrosby@camgian.com>
2024-09-15 16:51:03 -0400

    Traits and closures example

--- examples/trait_closure.rs
+++ examples/trait_closure.rs
@@ -1,78 +1,61 @@
 // Experiment with closure containing references
 
-struct s1<'a> {
+trait T1 {
+    fn callit(&self, y: usize) -> usize;
+}
 
+struct S1<'a> {
     str1: &'a String,
+}    
 
+impl<'b> T1 for S1<'b> {
+    fn callit(&self, y :usize) -> usize {
+        y + self.str1.len()
+    }
 }
 
-struct s2<'c> {
+struct S2<'c> {
+    tref: &'c dyn Fn (usize) -> usize,
+}        
 
-    tref: &'c dyn Fn (u32) -> u32,
-}
-
-trait t1 {
-
-    fn callit(&self, y: u32) -> u32;
-
-}
-
-impl<'b> t1 for s2<'b> {
-
-    fn callit(&self, y: u32) -> u32 {
+impl<'b> T1 for S2<'b> {
+    fn callit(&self, y: usize) -> usize {
         (self.tref)(y)
-    }
-
-}
-
-impl<'b> t1 for s1<'b> {
-
-    fn callit(&self, y :u32) -> u32 {
-        self.str1.len() as u32
-    }
-
-}
+    }    
+}    
 
 fn main() {
-
-    let sv1 = | x:u32 | -> u32 { x * x }; 
     
+    let s1a = S1{str1: &"blah".to_string()};
+    let tv: &dyn T1 = &s1a;
+    println!("s1a: {}", tv.callit(32));
+    
+    let sv1 = | x:usize | -> usize { x * x }; 
     let b1 = Box::new(sv1);
+    let sv3 = S2{tref: &b1};
     
-    let b2 = Box::new(| x:u32 | -> u32 { x * x }); 
-
+    let b2 = Box::new(| x:usize | -> usize { x * x }); 
+    let sv4 = S2{tref: &b2};
+    
     let str1 = "Hello World";
-
-    let b3 = | x: u32 | -> u32 { 
-
-        x + str1.len() as u32
-
+    let b3 = | x: usize | -> usize { 
+        x + str1.len() as usize
       };
-
-    let sv3 = s2{tref: &b1};
-    let sv4 = s2{tref: &b2};
-    let sv5 = s2{tref: &b3};
-
-    println!("{}", (sv5.tref)(32));
+    let sv5 = S2{tref: &b3};
     
-    to_trait(&sv3);
-    to_trait(&sv4);
-    to_trait(&sv5);
-
-    let s1a = s1{str1: &"blah".to_string()};
-
-    let tv: &dyn t1 = &s1a;
-    println!("{}", tv.callit(32));
-
-
+    println!("sv5: {}", (sv5.tref)(32));
+    
+    let tv = to_trait(&sv3);
+    println!("sv3: {}", tv.callit(10));
+    
+    let tv = to_trait(&sv4);
+    println!("sv4: {}", tv.callit(20));
+    
+    let tv = to_trait(&sv5);
+    println!("sv5: {}", tv.callit(30));
 
 }
 
-fn to_trait<'a>(s_in: &'a s2<'a>) -> &'a dyn t1 {
-
-    let tv: &dyn t1 = s_in;
-    println!("{}", tv.callit(32));
-
-    tv
-
+fn to_trait<'a>(s_in: &'a S2<'a>) -> &'a dyn T1 {
+    s_in
 }
